66c49846d0a12795b7dbc1d4770e1dc673a52c74
syamaws <101620003+syamaws@users.noreply.github.com>
2025-10-08 12:27:00 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Flask + Docker + Kubernetes + Jenkins on port 5001!"
+    return "Hello from Flask + Docker + Kubernetes + Jenkins!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=5000)
